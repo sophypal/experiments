c905b2ac6a7572248f6f7570a8553191a7da03d1
Sophy Pal <sophy.pal@gmail.com>
2021-04-29 02:47:15 -0700

    Update production path

--- config/environment.js
+++ config/environment.js
@@ -31,6 +31,10 @@ module.exports = function (environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
+  if (environment === 'production') {
+    ENV.rootURL = '/experiments/'
+  }
+
   if (environment === 'test') {
     // Testem prefers this...
     ENV.locationType = 'none';

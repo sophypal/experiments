85dec7554e1c6b7701f88de0b6e5103ee7a299b9
Sophy Pal <sophy.pal@gmail.com>
2021-04-29 02:42:12 -0700

    Update ignore and +x publish script

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TMP_DEMO_DIR=.gh-pages
+
+ember build -e production
+git clone https://github.com/sophypal/experiments.git ${TMP_DEMO_DIR}
+cd ${TMP_DEMO_DIR}
+git checkout gh-pages
+git rm -rf *
+cp -r ../dist/* .
+git add --all
+git commit -m "gh-pages publish"
+git push origin gh-pages > /dev/null 2>&1
